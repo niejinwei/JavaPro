2fa5b81eb141db926400f1bd741dcfeaa3dad021
niejinwei <1334564625@qq.com>
2018-07-14 13:05:20 +0800

    Hello,MyEclipse.

---
+++ JavaPro/src/com/java/test/JavaTest.java
@@ -0,0 +1,11 @@
+package com.java.test;
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello,MyEclipse 2017 CI 5");
+	}
+
+}
